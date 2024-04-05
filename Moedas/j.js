fc5ca8c68ac120dbd7e07022ad43541597607e55
Vitallzin <163913876+Vitallzin@users.noreply.github.com>
2024-04-05 11:36:55 -0300

    j.js

--- Moedas/j.js
+++ Moedas/j.js
@@ -1,35 +1,36 @@
-const API_KEY = "https://economia.awesomeapi.com.br/json/last/:moedas"; // Substitua por sua chave da API
+const API_KEY = "https://api.exchangerate-api.com/v4/latest/"; // Chave API
 
-const moedas = {
+const moedas = { // constante das moedas que vai mapear os codigos
     BRL: "Real Brasileiro",
     USD: "Dólar Americano",
     EUR: "Euro",
-    // Adicione outras moedas aqui com seus códigos e nomes
+    
 };
 
 function converterMoeda(moedaOrigem, valor, moedaDestino) {
     const url = `https://api.exchangerate-api.com/v4/latest/${moedaOrigem}`;
 
-    fetch(url)
+    fetch(url) //para realizar uma requisição à API usando a URL construída. 
         .then((response) => response.json())
         .then((data) => {
             const taxaCambio = data.rates[moedaDestino];
-            const valorConvertido = valor * taxaCambio;
+            const valorConvertido = valor * taxaCambio;// aqui onde acontece o calculo para saber o quanto equivale uma moeda para outra
 
-            const resultado = document.getElementById("resultado");
+            const resultado = document.getElementById("resultado"); // onde vai aparecer o resultado
             resultado.innerHTML = `
                 ${valor} ${moedas[moedaOrigem]} equivalem a:
-                **${valorConvertido.toFixed(2)} ${moedas[moedaDestino]}**
+                ${valorConvertido.toFixed(2)} ${moedas[moedaDestino]}
             `;
         });
 }
 
-document.addEventListener("submit", (event) => {
-    event.preventDefault();
+document.addEventListener("submit", (event) => {//ele vai ser acionado depois do usuario cliaar do botão "cpnverter"
+    event.preventDefault(); // previne que a pagijna seja recarregada permitindo que o código JavaScript lide com o envio do formulário
 
     const moedaOrigem = document.getElementById("moeda_origem").value;
     const valor = parseFloat(document.getElementById("valor").value);
-    const moedaDestino = document.getElementById("moeda_destino").value;
+    const moedaDestino = document.getElementById("moeda_destino").value; // aqui onde os dados serão captados apos o usuario mandar o formulário
 
     converterMoeda(moedaOrigem, valor, moedaDestino);
 });
+
