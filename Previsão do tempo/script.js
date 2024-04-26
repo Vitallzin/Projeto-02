b4ee12055ae065b3b4caa932ae122c41fdc29cfb
Vitallzin <163913876+Vitallzin@users.noreply.github.com>
2024-04-26 18:02:34 -0300

    script.js

--- Previsão do tempo/script.js
+++ Previsão do tempo/script.js
@@ -1,12 +1,36 @@
-//Variáveis e seleções de elementos
+//Variáveis e seleções de elementos https://api.openweathermap.org/data/2.5/weather?q=Paulista&units=metric&appid=9ce073af08c2e903acdc9d1730b91999
+const apiKey = "9ce073af08c2e903acdc9d1730b91999";
 const cityInput = document.querySelector("#city-input");
-const searchBt = document.querySelector("#search")
-
-
+const searchBt = document.querySelector("#search");
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
 
 
 
 //funções
+const getWeatherData = async(city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    return data;
+
+};
+
+
+const showWeatherData = async(city) => {
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+};
 
 
 
@@ -16,12 +40,8 @@ const searchBt = document.querySelector("#search")
 //Eventos
 searchBt.addEventListener("click" , (e) => {
     e.preventDefault(); //evita enviar o formulário
-    console.log("teste");
 
     const city = cityInput.value;
-    console.log(city);
+    showWeatherData(city);
 })
-
-
-
 
